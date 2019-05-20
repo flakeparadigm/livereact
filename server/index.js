03011f3ac9cf50c5ae84c12f991221f47442be92
Trungtin Ton <tin.developer@gmail.com>
2019-05-21 08:16:51 +1000

    Clean up comments, unused code

--- server/index.js
+++ server/index.js
@@ -20,10 +20,8 @@ let
 
 server.on("connection", (socket) => {
     console.info(`Client connected [id=${socket.id}]`);
-    // initialize this client's sequence number
     clientConnections.set(socket, socket.id);
 
-    // when socket disconnects, remove it from the list:
     socket.on("disconnect", () => {
         clientConnections.delete(socket);
         console.info(`Client gone [id=${socket.id}]`);
@@ -41,21 +39,10 @@ app.use(express.static('public'));
 
 app.post('/react', (req, res) => {
     for (const [client] of clientConnections.entries()) {
-        client.emit("reaction", req.body);
+        client.emit('reaction', req.body);
     }
     // Send a meaningful response payload
     res.json({ status: 'OK' });
 });
 
 app.listen(3000, () => console.log('server started'));
-
-
-
-// -------------------------------------------
-
-// CLIENT-SIDE (listening for reaction events)
-
-
-// WEB-SOCKET client connection
-// const electronApp = ioclient.connect("ws://10.20.8.171:8800");
-// electronApp.on("reaction", (msg) => console.info(msg));
