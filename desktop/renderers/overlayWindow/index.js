6c9d73a5c90eff257a5c48ea76c3d2407d688b46
John Ed Alvinez <je.alvinez@gmail.com>
2019-05-21 14:22:46 +1000

    Add multiple animations for emojis

--- desktop/renderers/overlayWindow/index.js
+++ desktop/renderers/overlayWindow/index.js
@@ -9,8 +9,10 @@ ipc.on('show-reaction', (event, reaction) => {
     const emojiEl = document.createElement('span');
     const maxVertical = emojiContainerEl.getBoundingClientRect().height - 200
     const getRandom = (min, max) => Math.floor(Math.random()*(max-min+1)+min);
+    const animations = ['animation-curve', 'animation-rotate', 'animation-scale']
 
     emojiEl.classList.add('emoji');
+    emojiEl.classList.add(animations[getRandom(0, animations.length - 1)]);
     emojiEl.innerText= reaction.content;
     emojiEl.style.top = getRandom(0, maxVertical)+'px';
     emojiEl.style.left = -100;
