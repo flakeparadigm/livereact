7573c193be246867825b9b6c90a5a4022914e3b3
John Ed Alvinez <je.alvinez@gmail.com>
2019-05-21 11:40:27 +1000

    Add emoji animation

--- desktop/renderers/overlayWindow/index.js
+++ desktop/renderers/overlayWindow/index.js
@@ -1,18 +1,21 @@
 // fire emojis
 const { ipcRenderer: ipc } = require('electron');
 const emojiContainerEl = document.getElementById('emojiContainer');
-let nextId = 0;
 
-const displayTime = 5000
+const displayTime = 3000
 
 ipc.on('show-reaction', (event, reaction) => {
     console.log(reaction);
     const emojiEl = document.createElement('span');
+
     emojiEl.classList.add('emoji');
     emojiEl.innerText= reaction.content;
     emojiEl.style.top = 100;
     emojiEl.style.left = 100;
 
+    const getRandom = (min, max) => Math.floor(Math.random()*(max-min+1)+min);
+
+    emojiContainerEl.style.top = getRandom(100, 700)+'px';
     emojiContainerEl.appendChild(emojiEl);
 
     setTimeout(() => {
