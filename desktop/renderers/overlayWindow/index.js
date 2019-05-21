5cbed36b05525c14adfeaab2e6f6baca978beb97
John Ed Alvinez <je.alvinez@gmail.com>
2019-05-21 12:58:15 +1000

    Update vertical positioning for emojis

--- desktop/renderers/overlayWindow/index.js
+++ desktop/renderers/overlayWindow/index.js
@@ -7,11 +7,12 @@ const displayTime = 3000
 ipc.on('show-reaction', (event, reaction) => {
     console.log(reaction);
     const emojiEl = document.createElement('span');
+    const maxVertical = emojiContainerEl.getBoundingClientRect().height - 200
     const getRandom = (min, max) => Math.floor(Math.random()*(max-min+1)+min);
 
     emojiEl.classList.add('emoji');
     emojiEl.innerText= reaction.content;
-    emojiEl.style.top = getRandom(15, 75)+'%';
+    emojiEl.style.top = getRandom(100, maxVertical)+'px';
     emojiEl.style.left = 100;
 
     emojiContainerEl.appendChild(emojiEl);
