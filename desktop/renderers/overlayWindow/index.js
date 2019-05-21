b508570d4934007ef9a9ba7d8b68eed5972fee01
John Ed Alvinez <je.alvinez@gmail.com>
2019-05-21 12:20:59 +1000

    Fix random vertical position for emojis

--- desktop/renderers/overlayWindow/index.js
+++ desktop/renderers/overlayWindow/index.js
@@ -7,15 +7,13 @@ const displayTime = 3000
 ipc.on('show-reaction', (event, reaction) => {
     console.log(reaction);
     const emojiEl = document.createElement('span');
+    const getRandom = (min, max) => Math.floor(Math.random()*(max-min+1)+min);
 
     emojiEl.classList.add('emoji');
     emojiEl.innerText= reaction.content;
-    emojiEl.style.top = 100;
+    emojiEl.style.top = getRandom(15, 75)+'%';
     emojiEl.style.left = 100;
 
-    const getRandom = (min, max) => Math.floor(Math.random()*(max-min+1)+min);
-
-    emojiContainerEl.style.top = getRandom(100, 700)+'px';
     emojiContainerEl.appendChild(emojiEl);
 
     setTimeout(() => {
